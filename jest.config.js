52f8d71490fa0bb4721abf227accf0c71f927353
Technote <technote.space@gmail.com>
2020-02-05 02:02:00 +0900

    chore: suppress diagnostics error

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
 module.exports = {
 	clearMocks: true,
-	moduleFileExtensions: [ 'js', 'ts' ],
-	setupFiles: [ '<rootDir>/jest.setup.ts' ],
+	moduleFileExtensions: ['js', 'ts'],
+	setupFiles: ['<rootDir>/jest.setup.ts'],
 	testEnvironment: 'node',
-	testMatch: [ '**/*.test.ts' ],
+	testMatch: ['**/*.test.ts'],
 	testRunner: 'jest-circus/runner',
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
 	},
 	verbose: true,
 	coverageDirectory: '<rootDir>/coverage',
+	globals: {
+		'ts-jest': {
+			diagnostics: false,
+		},
+	},
 };
