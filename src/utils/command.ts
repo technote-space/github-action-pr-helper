ff5233b52bb132b59cebab649aba99985cb44bc8
Technote <technote.space@gmail.com>
2020-03-17 02:54:50 +0900

    chore: tweaks

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -6,7 +6,6 @@ import {
 	isDisabledDeletePackage,
 	filterExtension,
 	getPrHeadRef,
-	getPrBaseRef,
 	getContextBranch,
 	getGitFilterStatus,
 	getCacheKey,
@@ -341,7 +340,7 @@ export const getChangedFilesForRebase = async(helper: GitHelper, logger: Logger,
 	output: CommandOutput[];
 }> => {
 	await initDirectory(helper, logger, context);
-	await helper.cloneBranch(getWorkspace(), getPrBaseRef(context), context.actionContext);
+	await helper.cloneBranch(getWorkspace(), getContextBranch(context), context.actionContext);
 	await helper.createBranch(getWorkspace(), await getPrBranchName(helper, octokit, context));
 
 	return runCommands(helper, logger, context);
