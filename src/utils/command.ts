12a2bcfd379d47d8ad12e199fed259a204e43c90
Technote <technote.space@gmail.com>
2020-12-14 02:24:27 +0900

    fix: base branch when update pr

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -7,7 +7,6 @@ import {components} from '@octokit/openapi-types';
 import {
   getActionDetail,
   isDisabledDeletePackage,
-  getPrBaseRef,
   getPrHeadRef,
   getContextBranch,
   getGitFilterStatus,
@@ -32,7 +31,7 @@ const {getWorkspace, getLocalRefspec, getRefspec} = Utils;
 const {getOctokit, ensureNotNullValue, useNpm}    = Utils;
 const {getRepository, isPush}                     = ContextHelper;
 
-export const getApiHelper = (octokit: Octokit, context: ActionContext, logger?: Logger, refForUpdate?: string): ApiHelper => new ApiHelper(octokit, context.actionContext, logger, {refForUpdate});
+export const getApiHelper = (octokit: Octokit, context: ActionContext, logger?: Logger): ApiHelper => new ApiHelper(octokit, context.actionContext, logger);
 
 export const clone = async(helper: GitHelper, logger: Logger, octokit: Octokit, context: ActionContext): Promise<void> => {
   const branchName = await getPrBranchName(helper, octokit, context);
@@ -369,7 +368,7 @@ export const resolveConflicts = async(branchName: string, helper: GitHelper, log
     }
     await commit(helper, logger, context);
     await forcePush(branchName, helper, logger, context);
-    await getApiHelper(getOctokit(getApiToken()), context, logger, getPrBaseRef(context)).pullsCreateOrUpdate(branchName, {
+    await getApiHelper(getOctokit(getApiToken()), context, logger).pullsCreateOrUpdate(branchName, {
       title: await getPrTitle(helper, octokit, context),
       body: await getPrBody(false, files, output, helper, octokit, context),
     });
