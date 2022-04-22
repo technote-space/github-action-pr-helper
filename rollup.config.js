505a648e8d241fca78ab5356aeeb57701cf7500a
Technote <technote.space@gmail.com>
2022-04-23 00:44:58 +0900

    chore: cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,26 @@
 import pluginTypescript from '@rollup/plugin-typescript';
 
-export default {
+const common = {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/index.mjs',
-    format: 'es',
-  },
   plugins: [
     pluginTypescript(),
   ],
   external: ['@actions/core', '@actions/github/lib/context', '@technote-space/github-action-helper', '@technote-space/github-action-log-helper', '@technote-space/filter-github-action', 'moment'],
 };
+
+export default [
+  {
+    ...common,
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+  },
+  {
+    ...common,
+    output: {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
+  },
+];
